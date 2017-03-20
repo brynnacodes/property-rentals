37bab8273f0a4c37e091f80b9f55e64ec86a2d1f
Brynna <brynna.mccarter@gmail.com>
2017-03-20 11:53:19 -0700

    add everything

---
+++ app/routes/index.js
@@ -0,0 +1,31 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model() {
+    return this.store.findAll('rental');
+  },
+
+  actions: {
+    saveRental3(params) {
+      var newRental = this.store.createRecord('rental', params);
+      newRental.save();
+      this.transitionTo('index');
+    },
+
+    update(rental, params) {
+      Object.keys(params).forEach(function(key) {
+        if(params[key]!==undefined) {
+          rental.set(key,params[key]);
+        }
+      });
+      rental.save();
+      this.transitionTo('index');
+    },
+
+    destroyRental(rental) {
+      rental.destroyRecord();
+      this.transitionTo('index');
+    }
+  }
+});
+//a method within an Ember class is called a hook. This model hook returns our hard coded array of rentals when we render our index.hbs template. When we refer to model in index.hbs we are referring to this array in this route-handler.
